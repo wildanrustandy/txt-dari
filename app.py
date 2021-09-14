178600c28afc5b5d93dcada65023543d6882fe2b
wildan.artsys <wildan.artsys@gmail.com>
2021-09-14 13:23:36 +0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,54 @@
+#Importing modules/libraries
+import tweepy
+from twitter import Twitter
+import constants
+import time
+
+
+tw = Twitter()
+keyword = 'ask' or 'tanya'
+
+def start():
+    print("Starting program...")
+    dms = list()
+    while True:
+        if len(dms) is not 0:
+            for i in range(len(dms)):
+                message = dms[i]['message']
+                # I take sender_id just in case you want to know who's sent the message
+                sender_id = dms[i]['sender_id']
+                id = dms[i]['id']
+
+                if len(message) is not 0 and len(message) < 280:
+                    # prikitiw is the keyword
+                    # if you want to turn off the case sensitive like: priktiw, Prikitiw, pRiKiTiw
+                    # just use lower(message) and check it, but please remove the replace function line
+                    if keyword.lower() in message:
+                        message = message.replace(keyword, "")
+                        if len(message) is not 0:
+                            if dms[i]['media'] is None:
+                                print("DM will be posted")
+                                tw.post_tweet(message)
+                                tw.delete_dm(id)
+                            else:
+                                print("DM will be posted with media")
+                                print(dms[i]['shorted_media_url'])
+                                tw.post_tweet_with_media(message, dms[i]['media'],dms[i]['shorted_media_url'], dms[i]['type'])
+                                tw.delete_dm(id)
+                        else:
+                            print("DM deleted because its empty..")
+                            tw.delete_dm(id)
+                    else:
+                        print("DM will be deleted because does not contains keyword..")
+                        tw.delete_dm(id)
+
+            dms = list()
+
+        else:
+            print("Direct message is empty...")
+            dms = tw.read_dm()
+            if len(dms) is 0 or dms is None:
+                time.sleep(60)
+
+if __name__ == "__main__":
+    start()
